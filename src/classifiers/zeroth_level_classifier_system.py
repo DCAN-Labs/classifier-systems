08d037bb956cad5be506770b26b157477bb7e191
Paul Reiners <reine097@umn.edu>
2024-05-24 10:56:14 -0500

    Finished constructor for Woods1Detector.

--- src/classifiers/zeroth_level_classifier_system.py
+++ src/classifiers/zeroth_level_classifier_system.py
@@ -1,17 +1,18 @@
 class ZerothLevelClassifierSystem:
-    def __init__(self, environment, classifiers, detector):
+    def __init__(self, environment, classifiers_file, detector):
         self.environment = environment
-        self.classifiers = classifiers
+        self.classifiers = self.get_classifiers(classifiers_file)
         self.detector = detector
 
     def done(self):
         pass
 
-    def run_one_cycle(self):
-        old_action_set = []
-        while not self.done():
-            features = self.detector.get_encoded_features()
-            match_set = self.classifiers.get_match_set(features)
-            highest_bidders = match_set.get_highest_bidders()
-            action_set = highest_bidders.get_action_set()
-            old_action_set = old_action_set.pay_highest_bidders(action_set)
+    def run_one_cycle(self, old_action_set):
+        features = self.detector.get_encoded_features()
+        match_set = self.classifiers.get_match_set(features)
+        highest_bidders = match_set.get_highest_bidders()
+        action_set = highest_bidders.get_action_set()
+        old_action_set = old_action_set.pay_highest_bidders(action_set)
+
+    def get_classifiers(self, classifiers_file):
+        pass
--- test/classifiers/test_zeroth_level_classifier_system.py
+++ test/classifiers/test_zeroth_level_classifier_system.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
+from src.environments.woods1 import Woods1
+
+from src.classifiers.zeroth_level_classifier_system import ZerothLevelClassifierSystem
+from detectors.woods1_detector import Woods1Detector
+
 
 class TestZerothLevelClassifierSystem(TestCase):
     def test_run_one_cycle(self):
-        self.fail()
+        environment = Woods1('./data/woods1.txt')
+        classifiers_file = './data/woods1_classifiers.txt'
+        detector = Woods1Detector(environment)
+        zeroth_level_classifier_system = ZerothLevelClassifierSystem(environment, classifiers_file, detector)
+        zeroth_level_classifier_system.run_one_cycle([])
