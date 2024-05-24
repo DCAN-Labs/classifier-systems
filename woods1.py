245838fd8a9c1b8d137d457f1391a3488c790d9f
Paul Reiners <reine097@umn.edu>
2024-05-24 09:14:21 -0500

    Parse world file.

--- woods1.py
+++ woods1.py
@@ -6,8 +6,24 @@ class Woods1(Environment):
         self.environment_file = environment_file
         with open(self.environment_file) as file:
             lines = [line.rstrip() for line in file]
-            for line in lines:
+            for i in range(len(lines)):
+                line = lines[i]
                 if line.startswith('#'):
                     continue
-                self.width, self.height = [int(part) for part in line.split()]
-                print()
+                else:
+                    parts = line.split()
+                    print(f'parts: {parts}')
+                    self.width = int(parts[0])
+                    self.height = int(parts[1])
+                    self.world = [["" for i in range(self.width)] for j in range(self.height)]
+                    print(f'world: {self.world}')
+                    i += 2
+                    for row in range(self.height):
+                        line = lines[i]
+                        print(f'line: {line}')
+                        for col in range(self.width):
+                            self.world[row][col] = line[col]
+                        i += 1
+                    print(f'self.world: {self.world}')
+                    break
+
