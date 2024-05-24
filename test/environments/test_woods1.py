4dbd174113b23c91efff0cd8b86d1b6fa0566171
Paul Reiners <reine097@umn.edu>
2024-05-24 09:50:01 -0500

    Reorganize.

--- test/environments/test_woods1.py
+++ test/environments/test_woods1.py
@@ -1,13 +1,13 @@
 import unittest
 
-from src.woods1 import Woods1
+from src.environments.woods1 import Woods1
 
 
 class TestWoods1Methods(unittest.TestCase):
 
     def test_init(self):
         woods1 = Woods1('data/woods1.txt')
-        print(woods1)
+        self.assertIsNotNone(woods1)
 
 
 if __name__ == '__main__':
